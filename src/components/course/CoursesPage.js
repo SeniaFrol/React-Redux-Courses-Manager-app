72e737d31ee6666428957dea992fa290c3d10d2e
Senia <senia.frolov@gmail.com>
2017-03-10 15:48:37 +0300

    add first action: add new course

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,13 +1,53 @@
 import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { createCourse } from '../../actions/courseActions';
 
 class CoursesPage extends Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {
+      course: { title: "" }
+    };
+  }
+
+  onTitleChange = (e) => {
+    const course = this.state.course;
+    course.title = e.target.value;
+    this.setState({ course });
+  }
+
+  onClickSave = () => {
+    this.props.createCourse(this.state.course);
+  }
+
+  courseRow = (course, index) => {
+    return <div key={index}>{course.title}</div>;
+  }
+
   render() {
     return(
       <div>
         <h1>Courses</h1>
+        {this.props.courses.map(this.courseRow)}
+        <h2>Add coure</h2>
+        <input
+          type="text"
+          onChange={this.onTitleChange}
+          value={this.state.course.title} />
+
+        <input
+          type="submit"
+          value="Save"
+          onClick={this.onClickSave} />
       </div>
     );
   }
 }
 
-export default CoursesPage;
+function mapStateToProps({courses}, ownProps) {
+  return {courses};
+}
+
+
+export default connect(mapStateToProps, { createCourse })(CoursesPage);
